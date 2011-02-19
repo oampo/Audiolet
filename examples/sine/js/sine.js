3dd6b67cf17ce59b6e92bf2aba87405fae009465
Joe Turner <joe@oampo.co.uk>
2011-02-19 10:17:00 +0000

    Modify sine example to put a panner before the output.  Will be fixed with an upmixer.

--- examples/sine/js/sine.js
+++ examples/sine/js/sine.js
@@ -1,7 +1,9 @@
 window.addEvent("domready", function() {
     var audiolet = new Audiolet(44100, 2, Math.pow(2, 13));
     var sine = new Sine(audiolet);
-    sine.connect(audiolet.output);
+    var pan = new Pan(audiolet);
+    sine.connect(pan);
+    pan.connect(audiolet.output);
 });
 
 
