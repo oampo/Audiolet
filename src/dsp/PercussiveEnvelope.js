ddd94af2eeb917f83a2bd12c9c6bbe3ee6639ba2
Joe Turner <joe@oampo.co.uk>
2011-02-25 10:08:04 +0000

    Add percussive envelope

---
+++ src/dsp/PercussiveEnvelope.js
@@ -0,0 +1,13 @@
+/**
+ * @depends Envelope.js
+ */
+var PercussiveEnvelope = new Class({
+    Extends: Envelope,
+    initialize: function(audiolet, gate, attack, release, onComplete) {
+        var levels = [0, 1, 0];
+        var times = [attack, release];
+        Envelope.prototype.initialize.apply(this, [audiolet, gate, levels,
+                                                   times, null,  onComplete]);
+    }
+});
+
