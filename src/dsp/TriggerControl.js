5ea099d7867c2648273f95cb3d86b2949dfdd018
Joe Turner <joe@oampo.co.uk>
2011-03-10 12:42:45 +0000

    Oops - forgot to add trigger control.  Been using it for ages too...

---
+++ src/dsp/TriggerControl.js
@@ -0,0 +1,35 @@
+/**
+ * @depends ../core/AudioletNode.js
+ */
+
+var TriggerControl = new Class({
+    Extends: AudioletNode,
+    initialize: function(audiolet, trigger) {
+        AudioletNode.prototype.initialize.apply(this, [audiolet, 0, 1]);
+        this.trigger = new AudioletParameter(this, null, trigger || 0);
+    },
+
+    generate: function(inputBuffers, outputBuffers) {
+        var buffer = outputBuffers[0];
+        var channel = buffer.getChannelData(0);
+
+        var triggerParameter = this.trigger;
+        var trigger = triggerParameter.getValue();
+
+        var bufferLength = buffer.length;
+        for (var i = 0; i < bufferLength; i++) {
+            if (trigger) {
+                channel[i] = 1;
+                triggerParameter.setValue(0);
+                trigger = 0;
+            }
+            else {
+                channel[i] = 0;
+            }
+        }
+    },
+
+    toString: function() {
+        return 'TriggerControl';
+    }
+});
