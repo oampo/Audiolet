d07f484d3fd4a573b28c6cc3c5454f8a9c2e9732
Joe Turner <joe@oampo.co.uk>
2011-03-11 13:52:50 +0000

    Add crappy but working aiff and wav reading to the audiolet buffer.  The decoding really needs making more robust, and splitting off into a separate library.

--- src/core/AudioletBuffer.js
+++ src/core/AudioletBuffer.js
@@ -153,5 +153,126 @@ var AudioletBuffer = new Class({
         var buffer = new AudioletBuffer(this.numberOfChannels, this.length);
         buffer.set(this);
         return buffer;
+    },
+
+    // WAV and AIFF loading based on http://pastebin.com/XVptxYEC from
+    // yuri & ccliffe
+    load: function(path, async) {
+        if (typeof async == 'undefined' || async == null) {
+            async = true;
+        }
+        var request = new Request({
+            url: path,
+            async: async,
+            onSuccess: function(data) {
+                // TODO: Maybe should check header, rather than just believing
+                // the extension
+                var splitPath = path.split(".");
+                var extension = splitPath[splitPath.length - 1].toLowerCase();
+                if (extension == "wav") {
+                    this.loadWAVData(data);
+                }
+                else if (extension == "aiff" || extension == "aif") {
+                    this.loadAIFFData(data);
+                }
+                else {
+                    console.error("Cannot load ." + extension + " files");
+                }
+                    
+            }.bind(this),
+
+            onFailure: function(xhr) {
+                console.error("Could not load", path);
+            }.bind(this)
+        });
+        request.xhr.overrideMimeType('text/plain; charset=x-user-defined');
+        request.get();
+    },
+
+    loadWAVData: function(data) {
+        // TODO: More robust loading!
+        // Ignore header - 12 bytes
+        // fmt chunk
+        // Ignore fmt - 4 bytes
+        // Ignore length of fmt - 4 bytes
+        // Ignore file encoding
+        // Number of channels - 2 bytes
+        var numberOfChannels =   (data.charCodeAt(22) & 0xFF) |
+                                ((data.charCodeAt(23) & 0xFF) << 8);
+        // Ignore sample rate - 4 bytes
+        // Ignore bytes/sec - 4 bytes
+        // Ignore block align - 2 bytes
+        // Ignore bitrate - 2 bytes
+
+        // data chunk
+        // Ignore data - 4 bytes
+        // Data size - 4 bytes
+        var length =  (data.charCodeAt(40) & 0xFF)        |
+                     ((data.charCodeAt(41) & 0xFF) << 8)  |
+                     ((data.charCodeAt(42) & 0xFF) << 16) |
+                     ((data.charCodeAt(43) & 0xFF) << 24);
+        // 2 bytes per sample
+        length /= 2 * numberOfChannels;
+        this.resize(numberOfChannels, length);
+
+        var offset = 44;
+        for (var i = 0; i < numberOfChannels; i++) {
+            var channel = this.channels[i];
+            for (var j = 0; j < length; j++) {
+                var index = offset + (j * numberOfChannels + i) * 2;
+                // Sample - 2 bytes
+                var value =  (data.charCodeAt(index) & 0xFF) |
+                            ((data.charCodeAt(index + 1) & 0xFF) << 8);
+                // Scale range from 0 to 2**16 -> -2**15 to 2**15
+                if (value >= 0x8000) {
+                    value |= ~0x7FFF;
+                }
+                // Scale range to -1 to 1
+                channel[j] = value / 0x8000;
+            }
+        }
+    },
+
+    loadAIFFData: function(data) {
+        // TODO: More robust loading!
+        // Only loads 16 bit AIFF-Cs without comments, ignoring the sample rate
+        // Ignore the header - 16 bytes
+        // COMM Chunk
+        // Ignore COMM - 4 bytes
+        // Number of channels - 2 bytes
+        var numberOfChannels = ((data.charCodeAt(20) & 0xFF) << 8) |
+                                (data.charCodeAt(21) & 0xFF);
+        // Number of samples - 4 bytes
+        var length = ((data.charCodeAt(22) & 0xFF) << 24) |
+                     ((data.charCodeAt(23) & 0xFF) << 16) |
+                     ((data.charCodeAt(24) & 0xFF) << 8)  |
+                      (data.charCodeAt(25) & 0xFF);
+        this.resize(numberOfChannels, length, true);
+        
+        // Ignore bitrate - 2 bytes
+        // Ignore sample rate - 10 bytes
+
+        // SSND Chunk
+        // Ignore SSND - 4 bytes
+        // Ignore chunk size - 4 bytes
+        // Ignore offset - 4 bytes
+        // Ignore block size - 4 bytes
+        // Hope to hell that there isn't a comment - 0 bytes
+        var offset = 54;
+        for (var i = 0; i < numberOfChannels; i++) {
+            var channel = this.channels[i];
+            for (var j = 0; j < length; j++) {
+                var index = offset + (j * numberOfChannels + i) * 2;
+                // Sample - 2 bytes
+                var value = ((data.charCodeAt(index) & 0xFF) << 8) |
+                             (data.charCodeAt(index + 1) & 0xFF);
+                // Scale range from 0 to 2**16 -> -2**15 to 2**15
+                if (value >= 0x8000) {
+                    value |= ~0x7FFF;
+                }
+                // Scale range to -1 to 1
+                channel[j] = value / 0x8000;
+            }
+        } 
     }
 });
