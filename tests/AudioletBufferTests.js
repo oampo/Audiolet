a905e1569548bb408bb45d9f5666c9e9f7b8e09a
Joe Turner <joe@oampo.co.uk>
2011-03-31 16:42:08 +0100

    Add tests for interleaving and combining buffers

--- tests/AudioletBufferTests.js
+++ tests/AudioletBufferTests.js
@@ -393,5 +393,40 @@ function testShift() {
 
 test("Shift", testShift);
 
+function testInterleaved() {
+    var buffer = new AudioletBuffer(2, 2);
+    buffer.channels[0][0] = 1;
+    buffer.channels[0][1] = 2;
+    buffer.channels[1][0] = 3;
+    buffer.channels[1][1] = 5;
+
+    var interleaved = buffer.interleaved();
+    Assert.assertEquals(interleaved.length, 4, "Length");
+    Assert.assertEquals(interleaved[0], 1, "Value 1");
+    Assert.assertEquals(interleaved[1], 3, "Value 2");
+    Assert.assertEquals(interleaved[2], 2, "Value 3");
+    Assert.assertEquals(interleaved[3], 5, "Value 4");
+}
+
+test("Interleaved", testInterleaved);
+
+function testCombined() {
+    var buffer = new AudioletBuffer(2, 2);
+    buffer.channels[0][0] = 1;
+    buffer.channels[0][1] = 2;
+    buffer.channels[1][0] = 3;
+    buffer.channels[1][1] = 5;
+
+    var combined = buffer.combined();
+    Assert.assertEquals(combined.length, 4, "Length");
+    Assert.assertEquals(combined[0], 1, "Value 1");
+    Assert.assertEquals(combined[1], 2, "Value 2");
+    Assert.assertEquals(combined[2], 3, "Value 3");
+    Assert.assertEquals(combined[3], 5, "Value 4");
+}
+
+test("Combined", testCombined);
+
+
 
 
