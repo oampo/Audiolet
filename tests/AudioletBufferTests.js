77ff83a306f216e3c935a705a239af68e3e31a31
Joe Turner <joe@oampo.co.uk>
2011-03-31 14:40:53 +0100

    Add tests for buffer adding

--- tests/AudioletBufferTests.js
+++ tests/AudioletBufferTests.js
@@ -57,6 +57,59 @@ function testSetSection() {
 
 test("Set Section", testSetSection);
 
+function testAdd() {
+    var buffer1 = new AudioletBuffer(2, 2);
+    var buffer2 = new AudioletBuffer(2, 2);
+    buffer1.channels[0][0] = 1;
+    buffer1.channels[0][1] = 2;
+    buffer1.channels[1][0] = 3;
+    buffer1.channels[1][1] = 5;
+    buffer2.channels[0][0] = 8;
+    buffer2.channels[0][1] = 13;
+    buffer2.channels[1][0] = 21;
+    buffer2.channels[1][1] = 34;
+
+    buffer2.add(buffer1);
+    // Value from each channel
+    Assert.assertEquals(buffer2.channels[0][0], 9, "Value 1");
+    Assert.assertEquals(buffer2.channels[1][1], 39, "Value 2");
+}
+
+test("Add", testAdd);
+
+function testAddSection() {
+    var buffer1 = new AudioletBuffer(2, 4);
+    var buffer2 = new AudioletBuffer(2, 4);
+    buffer1.channels[0][0] = 1;
+    buffer1.channels[0][1] = 2;
+    buffer1.channels[0][2] = 3;
+    buffer1.channels[0][3] = 5;
+    buffer1.channels[1][0] = 8;
+    buffer1.channels[1][1] = 13;
+    buffer1.channels[1][2] = 21;
+    buffer1.channels[1][3] = 34;
+    buffer2.channels[0][0] = 55;
+    buffer2.channels[0][1] = 89;
+    buffer2.channels[0][2] = 1;
+    buffer2.channels[0][3] = 2;
+    buffer2.channels[1][0] = 3;
+    buffer2.channels[1][1] = 5;
+    buffer2.channels[1][2] = 8;
+    buffer2.channels[1][3] = 13;
+
+    buffer2.addSection(buffer1, 2, 2, 1);
+    // Value before adding section
+    Assert.assertEquals(buffer2.channels[0][0], 55, "Start Value");
+    // Value from each channel
+    Assert.assertEquals(buffer2.channels[0][1], 92, "Added Value 1");
+    Assert.assertEquals(buffer2.channels[1][2], 42, "Added Value 2");
+    // Value after adding section
+    Assert.assertEquals(buffer2.channels[1][3], 13, "Start Value");
+}
+
+test("Add Section", testAddSection);
+
+
 function testLazyResizeMoreSamples() {
     var buffer = new AudioletBuffer(1, 0);
     buffer.resize(1, 5, true);
