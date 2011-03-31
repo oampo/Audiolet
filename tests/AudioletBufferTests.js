6176529c6918146dc1517ef5f157df15fa0a0626
Joe Turner <joe@oampo.co.uk>
2011-03-31 14:17:29 +0100

    Add tests for buffer set functions

--- tests/AudioletBufferTests.js
+++ tests/AudioletBufferTests.js
@@ -15,6 +15,48 @@ function testInit() {
 
 test("Initialization", testInit);
 
+function testSet() {
+    var buffer1 = new AudioletBuffer(2, 3);
+    var buffer2 = new AudioletBuffer(2, 3);
+    buffer1.channels[0][0] = 1;
+    buffer1.channels[0][1] = 2;
+    buffer1.channels[0][2] = 3;
+    buffer1.channels[1][0] = 5;
+    buffer1.channels[1][1] = 8;
+    buffer1.channels[1][2] = 13;
+
+    buffer2.set(buffer1);
+    Assert.assertEquals(buffer2.channels[0][0], 1, "Start value");
+    Assert.assertEquals(buffer2.channels[1][1], 8, "Middle value");
+    Assert.assertEquals(buffer2.channels[1][2], 13, "End value");
+}
+
+test("Set", testSet);
+
+function testSetSection() {
+    var buffer1 = new AudioletBuffer(2, 4);
+    var buffer2 = new AudioletBuffer(2, 5);
+    buffer1.channels[0][0] = 1;
+    buffer1.channels[0][1] = 2;
+    buffer1.channels[0][2] = 3;
+    buffer1.channels[0][3] = 5;
+    buffer1.channels[1][0] = 8;
+    buffer1.channels[1][1] = 13;
+    buffer1.channels[1][2] = 21;
+    buffer1.channels[1][3] = 34;
+
+    buffer2.setSection(buffer1, 2, 1, 2);
+    // Zero at the start
+    Assert.assertEquals(buffer2.channels[0][0], 0, "Start value");
+    // Value from each channel
+    Assert.assertEquals(buffer2.channels[0][2], 2, "Copied value 1");
+    Assert.assertEquals(buffer2.channels[1][3], 21, "Copied value 2");
+    // Zero at the end
+    Assert.assertEquals(buffer2.channels[0][4], 0, "End value"); 
+}
+
+test("Set Section", testSetSection);
+
 function testLazyResizeMoreSamples() {
     var buffer = new AudioletBuffer(1, 0);
     buffer.resize(1, 5, true);
