6198225b91c713cf8e49978c48ef5fa48b51568d
Joe Turner <joe@oampo.co.uk>
2011-05-20 10:00:51 +0100

    Make scale automatically wrap high degrees to higher octaves.

--- src/scale/Scale.js
+++ src/scale/Scale.js
@@ -6,6 +6,8 @@ var Scale = new Class({
 
     getFrequency: function(degree, rootFrequency, octave) {
         var frequency = rootFrequency;
+        octave += Math.floor(degree / this.degrees.length);
+        degree %= this.degrees.length;
         frequency *= Math.pow(this.tuning.octaveRatio, octave);
         frequency *= this.tuning.ratios[this.degrees[degree]];
         return frequency;
