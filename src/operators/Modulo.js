8abffe6dbd26a98adc56e3e8e1bd91f43283990a
Joe Turner <joe@oampo.co.uk>
2011-05-25 11:44:52 +0100

    Add section of operators, for performing basic maths as nodes

---
+++ src/operators/Modulo.js
@@ -0,0 +1,50 @@
+/**
+ * @depends ../core/AudioletNode.js
+ */
+
+var Modulo = new Class({
+    Extends: AudioletNode,
+    initialize: function(audiolet, value) {
+        AudioletNode.prototype.initialize.apply(this, [audiolet, 2, 1]);
+        this.linkNumberOfOutputChannels(0, 0);
+        this.value = new AudioletParameter(this, 1, value || 1);
+    },
+
+    generate: function(inputBuffers, outputBuffers) {
+        var inputBuffer = inputBuffers[0];
+        var outputBuffer = outputBuffers[0];
+
+        if (inputBuffer.isEmpty) {
+            outputBuffer.isEmpty = true;
+            return;
+        }
+
+        // Local processing variables
+        var valueParameter = this.value;
+        var value, valueChannel;
+        if (valueParameter.isStatic()) {
+            value = valueParameter.getValue();
+        }
+        else {
+            valueChannel = valueParameter.getChannel();
+        }
+
+        var numberOfChannels = inputBuffer.numberOfChannels;
+        for (var i = 0; i < numberOfChannels; i++) {
+            var inputChannel = inputBuffer.getChannelData(i);
+            var outputChannel = outputBuffer.getChannelData(i);
+            var bufferLength = inputBuffer.length;
+            for (var j = 0; j < bufferLength; j++) {
+                if (valueChannel) {
+                    value = valueChannel[j];
+                }
+                outputChannel[j] = inputChannel[j] % value;
+            }
+        }
+    },
+
+    toString: function() {
+        return 'Modulo';
+    }
+});
+
