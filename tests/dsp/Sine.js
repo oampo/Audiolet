40ed55c1533028788b2f830fce17b6bd723c9acd
Joe Turner <joe@oampo.co.uk>
2011-05-31 11:49:53 +0100

    Add basic sine tests

---
+++ tests/dsp/Sine.js
@@ -0,0 +1,25 @@
+load('../../../../audiotest.js/trunk/audiotest.js');
+load('../mootools-core-1.3.1.js');
+load('../../src/audiofile/audiofile.js');
+load('../../src/audiolet/Audiolet.js');
+load('../Environment.js');
+
+function testSine() {
+    var audiolet = new Audiolet();
+    var sine = new Sine(audiolet);
+    var recorder = new InputRecorder(audiolet, 1);
+
+    sine.connect(recorder);
+
+    for (var i=0; i<10; i++) {
+        recorder.tick(8192, i);
+    }
+
+    var buffer = recorder.buffers[0];
+    var data = buffer.getChannelData(0);
+    Assert.assertContinuous(data);
+    Assert.assertAudibleValues(data);
+    Assert.assertValuesInRange(data);
+}
+
+test("Sine", testSine);
