71c7a4ad3d8c719e58f5b0f66250ce5f16a414fb
Eric Koslow <ekoslow@gmail.com>
2011-06-08 19:18:35 -0400

    Removed the test enviroment\'s dependancy on MooTools

--- tests/Environment.js
+++ tests/Environment.js
@@ -2,87 +2,78 @@ function Audio() {}
 
 function setInterval() {}
 
-var Introspector = new Class({
-    Extends: AudioletNode,
-    initialize: function(audiolet, numberOfInputs, numberOfOutputs) {
-        AudioletNode.prototype.initialize.apply(this, [audiolet, numberOfInputs,
-                                                       numberOfOutputs]);
-        this.timesCalled = 0;
-    },
+var Introspector = function(audiolet, numberOfInputs, numberOfOutputs) {
+  Introspector.superclass.call(this, audiolet, numberOfInputs,
+      numberOfOutputs);
+  this.timesCalled = 0;
+}
+extend(Introspector, AudioletNode); 
 
-    generate: function(inputBuffers, outputBuffers) {
-        this.inputBuffers = inputBuffers;
-        this.outputBuffers = outputBuffers;
-        this.timesCalled += 1;
+Introspector.prototype.generate = function(inputBuffers, outputBuffers) {
+  this.inputBuffers = inputBuffers;
+  this.outputBuffers = outputBuffers;
+  this.timesCalled += 1;
+}
+
+
+var ConstantSource = function(audiolet, numberOfOutputs, fillValue) {
+  ConstantSource.superclass.call(this, audiolet, 0, numberOfOutputs);
+  this.fillValue = fillValue || 1;
+}
+extend(ConstantSource, AudioletNode); 
+
+ConstantSource.prototype.generate = function(inputBuffers, outputBuffers) {
+  var fillValue = this.fillValue;
+  var numberOfBuffers = outputBuffers.length;
+  for (var i=0; i<numberOfBuffers; i++) {
+    var buffer = outputBuffers[i];
+    var numberOfChannels = buffer.numberOfChannels;
+    var bufferLength = buffer.length;
+    for (var j=0; j<numberOfChannels; j++) {
+      var data = buffer.getChannelData(j);
+      for (var k=0; k<bufferLength; k++) {
+        data[k] = fillValue;
+      }
     }
-});
+  }
+}
 
 
+var EmptySource = function(audiolet, numberOfOutputs) {
+  EmptySource.superclass.call(this, audiolet, 0, numberOfOutputs);
+}
+extend(EmptySource, AudioletNode); 
 
-var ConstantSource = new Class({
-    Extends: AudioletNode,
-    initialize: function(audiolet, numberOfOutputs, fillValue) {
-        AudioletNode.prototype.initialize.apply(this, [audiolet, 0,
-                                                       numberOfOutputs]);
-        this.fillValue = fillValue || 1;
-    },
+EmptySource.prototype.generate = function(inputBuffers, outputBuffers) {
+  var numberOfBuffers = outputBuffers.length;
+  for (var i=0; i<numberOfBuffers; i++) {
+    outputBuffers[i].isEmpty = true;
+  }
+}
 
-    generate: function(inputBuffers, outputBuffers) {
-        var fillValue = this.fillValue;
-        var numberOfBuffers = outputBuffers.length;
-        for (var i=0; i<numberOfBuffers; i++) {
-            var buffer = outputBuffers[i];
-            var numberOfChannels = buffer.numberOfChannels;
-            var bufferLength = buffer.length;
-            for (var j=0; j<numberOfChannels; j++) {
-                var data = buffer.getChannelData(j);
-                for (var k=0; k<bufferLength; k++) {
-                    data[k] = fillValue;
-                }
-            }
-        }
+
+var InputRecorder = function(audiolet, numberOfInputs) {
+  InputRecorder.superclass.call(this, audiolet, numberOfInputs, 0);
+  this.buffers = [];
+  for (var i=0; i<numberOfInputs; i++) {
+    this.buffers.push(new AudioletBuffer(1, 0));
+  }
+}
+extend(InputRecorder, AudioletNode); 
+
+InputRecorde.prototype.generate = function(inputBuffers, outputBuffers) {
+  var buffers = this.buffers;
+
+  for (var i=0; i<inputBuffers.length; i++) {
+    var buffer = inputBuffers[i];
+    var recordBuffer = buffers[i];
+    if (buffer.isEmpty) {
+      recordBuffer.push(new Float32Array(buffer.length));
     }
-});
-
-var EmptySource = new Class({
-    Extends: AudioletNode,
-    initialize: function(audiolet, numberOfOutputs) {
-        AudioletNode.prototype.initialize.apply(this, [audiolet, 0,
-                                                       numberOfOutputs]);
-    },
-
-    generate: function(inputBuffers, outputBuffers) {
-        var numberOfBuffers = outputBuffers.length;
-        for (var i=0; i<numberOfBuffers; i++) {
-            outputBuffers[i].isEmpty = true;
-        }
+    else {
+      recordBuffer.push(buffer);
     }
-});    
+  }
+}
 
-var InputRecorder = new Class({
-    Extends: AudioletNode,
-    initialize: function(audiolet, numberOfInputs) {
-        AudioletNode.prototype.initialize.apply(this, [audiolet, numberOfInputs,
-                                                       0]);
-        this.buffers = [];
-        for (var i=0; i<numberOfInputs; i++) {
-            this.buffers.push(new AudioletBuffer(1, 0));
-        }
-    },
-
-    generate: function(inputBuffers, outputBuffers) {
-        var buffers = this.buffers;
-
-        for (var i=0; i<inputBuffers.length; i++) {
-            var buffer = inputBuffers[i];
-            var recordBuffer = buffers[i];
-            if (buffer.isEmpty) {
-                recordBuffer.push(new Float32Array(buffer.length));
-            }
-            else {
-                recordBuffer.push(buffer);
-            }
-        }
-    }
-});
 
