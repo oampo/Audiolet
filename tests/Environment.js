8c61804ca51f32df54d3b4851be1766de68a1608
Joe Turner <joe@oampo.co.uk>
2011-06-09 13:53:11 +0100

    Retab and fix a typo in Environment

--- tests/Environment.js
+++ tests/Environment.js
@@ -3,77 +3,77 @@ function Audio() {}
 function setInterval() {}
 
 var Introspector = function(audiolet, numberOfInputs, numberOfOutputs) {
-  Introspector.superclass.call(this, audiolet, numberOfInputs,
-      numberOfOutputs);
-  this.timesCalled = 0;
+    Introspector.superclass.call(this, audiolet, numberOfInputs,
+            numberOfOutputs);
+    this.timesCalled = 0;
 }
 extend(Introspector, AudioletNode); 
 
 Introspector.prototype.generate = function(inputBuffers, outputBuffers) {
-  this.inputBuffers = inputBuffers;
-  this.outputBuffers = outputBuffers;
-  this.timesCalled += 1;
+    this.inputBuffers = inputBuffers;
+    this.outputBuffers = outputBuffers;
+    this.timesCalled += 1;
 }
 
 
 var ConstantSource = function(audiolet, numberOfOutputs, fillValue) {
-  ConstantSource.superclass.call(this, audiolet, 0, numberOfOutputs);
-  this.fillValue = fillValue || 1;
+    ConstantSource.superclass.call(this, audiolet, 0, numberOfOutputs);
+    this.fillValue = fillValue || 1;
 }
 extend(ConstantSource, AudioletNode); 
 
 ConstantSource.prototype.generate = function(inputBuffers, outputBuffers) {
-  var fillValue = this.fillValue;
-  var numberOfBuffers = outputBuffers.length;
-  for (var i=0; i<numberOfBuffers; i++) {
-    var buffer = outputBuffers[i];
-    var numberOfChannels = buffer.numberOfChannels;
-    var bufferLength = buffer.length;
-    for (var j=0; j<numberOfChannels; j++) {
-      var data = buffer.getChannelData(j);
-      for (var k=0; k<bufferLength; k++) {
-        data[k] = fillValue;
-      }
+    var fillValue = this.fillValue;
+    var numberOfBuffers = outputBuffers.length;
+    for (var i=0; i<numberOfBuffers; i++) {
+        var buffer = outputBuffers[i];
+        var numberOfChannels = buffer.numberOfChannels;
+        var bufferLength = buffer.length;
+        for (var j=0; j<numberOfChannels; j++) {
+            var data = buffer.getChannelData(j);
+            for (var k=0; k<bufferLength; k++) {
+                data[k] = fillValue;
+            }
+        }
     }
-  }
 }
 
 
 var EmptySource = function(audiolet, numberOfOutputs) {
-  EmptySource.superclass.call(this, audiolet, 0, numberOfOutputs);
+    EmptySource.superclass.call(this, audiolet, 0, numberOfOutputs);
 }
 extend(EmptySource, AudioletNode); 
 
 EmptySource.prototype.generate = function(inputBuffers, outputBuffers) {
-  var numberOfBuffers = outputBuffers.length;
-  for (var i=0; i<numberOfBuffers; i++) {
-    outputBuffers[i].isEmpty = true;
-  }
+    var numberOfBuffers = outputBuffers.length;
+    for (var i=0; i<numberOfBuffers; i++) {
+        outputBuffers[i].isEmpty = true;
+    }
 }
 
 
 var InputRecorder = function(audiolet, numberOfInputs) {
-  InputRecorder.superclass.call(this, audiolet, numberOfInputs, 0);
-  this.buffers = [];
-  for (var i=0; i<numberOfInputs; i++) {
-    this.buffers.push(new AudioletBuffer(1, 0));
-  }
+    InputRecorder.superclass.call(this, audiolet, numberOfInputs, 0);
+    this.buffers = [];
+    for (var i=0; i<numberOfInputs; i++) {
+        this.buffers.push(new AudioletBuffer(1, 0));
+    }
 }
 extend(InputRecorder, AudioletNode); 
 
-InputRecorde.prototype.generate = function(inputBuffers, outputBuffers) {
-  var buffers = this.buffers;
+InputRecorder.prototype.generate = function(inputBuffers, outputBuffers) {
+    var buffers = this.buffers;
 
-  for (var i=0; i<inputBuffers.length; i++) {
-    var buffer = inputBuffers[i];
-    var recordBuffer = buffers[i];
-    if (buffer.isEmpty) {
-      recordBuffer.push(new Float32Array(buffer.length));
+    for (var i=0; i<inputBuffers.length; i++) {
+        var buffer = inputBuffers[i];
+        var recordBuffer = buffers[i];
+        if (buffer.isEmpty) {
+            recordBuffer.push(new Float32Array(buffer.length));
+        }
+        else {
+            recordBuffer.push(buffer);
+        }
     }
-    else {
-      recordBuffer.push(buffer);
-    }
-  }
 }
 
 
