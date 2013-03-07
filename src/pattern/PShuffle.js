00705514baa4fa5601bef852da71682b254a525c
catshirt <caden@catshirt.net>
2013-03-07 00:12:25 -0500

    adding patterns to base class

--- src/pattern/PShuffle.js
+++ src/pattern/PShuffle.js
@@ -4,59 +4,63 @@
 
 /**
  * Reorder an array, then iterate through it's values.
- *
- * @constructor
- * @extends Pattern
- * @param {Object[]} list Array of values.
- * @param {Number} repeats Number of times to loop through the array.
  */
-var PShuffle = function(list, repeats) {
-    Pattern.call(this);
-    this.list = [];
-    // Shuffle values into new list
-    while (list.length) {
-        var index = Math.floor(Math.random() * list.length);
-        var value = list.splice(index, 1);
-        this.list.push(value);
-    }
-    this.repeats = repeats;
-    this.position = 0;
-};
-extend(PShuffle, Pattern);
+var PShuffle = Pattern.extend({
 
-/**
- * Generate the next value in the pattern.
- *
- * @return {Number} The next value.
- */
-PShuffle.prototype.next = function() {
-    var returnValue;
-    if (this.position < this.repeats * this.list.length) {
-        var index = (this.position + this.offset) % this.list.length;
-        var item = this.list[index];
-        var value = this.valueOf(item);
-        if (value != null) {
-            if (!(item instanceof Pattern)) {
-                this.position += 1;
+    /**
+     * Constructor
+     *
+     * @extends Pattern
+     * @param {Object[]} list Array of values.
+     * @param {Number} repeats Number of times to loop through the array.
+     */
+    constructor: function(list, repeats) {
+        Pattern.call(this);
+        this.list = [];
+        // Shuffle values into new list
+        while (list.length) {
+            var index = Math.floor(Math.random() * list.length);
+            var value = list.splice(index, 1);
+            this.list.push(value);
+        }
+        this.repeats = repeats;
+        this.position = 0;
+    },
+
+    /**
+     * Generate the next value in the pattern.
+     *
+     * @return {Number} The next value.
+     */
+    next: function() {
+        var returnValue;
+        if (this.position < this.repeats * this.list.length) {
+            var index = (this.position + this.offset) % this.list.length;
+            var item = this.list[index];
+            var value = this.valueOf(item);
+            if (value != null) {
+                if (!(item instanceof Pattern)) {
+                    this.position += 1;
+                }
+                returnValue = value;
             }
-            returnValue = value;
+            else {
+                if (item instanceof Pattern) {
+                    item.reset();
+                }
+                this.position += 1;
+                returnValue = this.next();
+            }
         }
         else {
-            if (item instanceof Pattern) {
-                item.reset();
-            }
-            this.position += 1;
-            returnValue = this.next();
+            returnValue = null;
         }
+        return (returnValue);
     }
-    else {
-        returnValue = null;
-    }
-    return (returnValue);
-};
+
+});
 
 /**
  * Supercollider alias
  */
 var Pshuffle = PShuffle;
-
