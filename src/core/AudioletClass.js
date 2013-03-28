168b2166d0bbbc0b6e23ab394a44a5c253e575b2
catshirt <caden@catshirt.net>
2013-03-27 22:59:11 -0400

    removing wrapper function

--- src/core/AudioletClass.js
+++ src/core/AudioletClass.js
@@ -6,79 +6,42 @@ var AudioletClass = function() {
 };
 
 /**
- * Create a "class method" for extending objects.
- * Stolen from Backbone.
+ * Create a class method for extending objects.
  */
-AudioletClass.extend = (function() {
+AudioletClass.extend = function(protoProps) {
 
-  var breaker = {};
+  var parent = this,
+    child;
 
-  var has = function(obj, key) {
-    return hasOwnProperty.call(obj, key);
-  };
+  // The constructor function for the new subclass is either defined by you
+  // (the "constructor" property in your `extend` definition), or defaulted
+  // by us to simply call the parent's constructor.
+  if (protoProps && protoProps.hasOwnProperty('constructor')) {
+    child = protoProps.constructor;
+  } else {
+    child = function(){ parent.apply(this, arguments); };
+  }
 
-  var each = function(obj, iterator, context) {
-    if (obj == null) return;
-    if (obj.length === +obj.length) {
-      for (var i = 0, l = obj.length; i < l; i++) {
-        if (i in obj && iterator.call(context, obj[i], i, obj) === breaker) return;
-      }
-    } else {
-      for (var key in obj) {
-        if (has(obj, key)) {
-          if (iterator.call(context, obj[key], key, obj) === breaker) return;
-        }
-      }
-    }
-  };
-
-  var _extend = function(obj) {
-    each(Array.prototype.slice.call(arguments, 1), function(source) {
-      for (var prop in source) {
-        obj[prop] = source[prop];
-      }
-    });
-    return obj;
-  };
-
-
-  // Shared empty constructor function to aid in prototype-chain creation.
+  // Set the prototype chain to inherit from `parent`, without calling
+  // `parent`'s constructor function.
   var ctor = function(){};
+  ctor.prototype = parent.prototype;
+  child.prototype = new ctor();
 
-  // Helper function to correctly set up the prototype chain, for subclasses.
-  // Similar to `goog.inherits`, but uses a hash of prototype properties and
-  // class properties to be extended.
-  var inherits = function(parent, protoProps) {
-    var child;
-
-    // The constructor function for the new subclass is either defined by you
-    // (the "constructor" property in your `extend` definition), or defaulted
-    // by us to simply call the parent's constructor.
-    if (protoProps && protoProps.hasOwnProperty('constructor')) {
-      child = protoProps.constructor;
-    } else {
-      child = function(){ parent.apply(this, arguments); };
+  // Add prototype properties (instance properties) to the subclass,
+  // if supplied.
+  for (var key in protoProps) {
+    if (protoProps.hasOwnProperty(key)) {
+      child.prototype[key] = protoProps[key];
     }
+  }
 
-    // Set the prototype chain to inherit from `parent`, without calling
-    // `parent`'s constructor function.
-    ctor.prototype = parent.prototype;
-    child.prototype = new ctor();
+  // Correctly set child's `prototype.constructor`.
+  child.prototype.constructor = child;
 
-    // Add prototype properties (instance properties) to the subclass,
-    // if supplied.
-    if (protoProps) _extend(child.prototype, protoProps);
+  // Expose `extend` method.
+  child.extend = this.extend;
 
-    // Correctly set child's `prototype.constructor`.
-    child.prototype.constructor = child;
+  return child;
 
-    return child;
-  };
-
-  return function (protoProps, classProps) {
-    var child = inherits(this, protoProps, classProps);
-    child.extend = this.extend;
-    return child;
-  };
-
-})();
+};
